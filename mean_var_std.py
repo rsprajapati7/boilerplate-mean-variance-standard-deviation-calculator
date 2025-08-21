e059b63bf6f182dff2d7b27ecd301dfda2b572d9
Ritvik Singh <ritvik.s.prajapati2007@gmail.com>
2025-08-21 05:46:54 +0000

    Project Completed

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,48 @@
 import numpy as np
 
-def calculate(list):
+def calculate(listt):
+    """
+    The input of the function should be a list containing 9 digits. 
+    The function should convert the list into a 3 x 3 Numpy array, 
+    and then return a dictionary containing the mean, variance, standard deviation, 
+    max, min, and sum along both axes and for the flattened matrix.
+    """
+    if len(listt) != 9:
+        raise ValueError('List must contain nine numbers.')
+    matrix = np.array(listt).reshape(3,3) # converts the listt into a 3x3 matrix
+    
+    calculations = {
+        'mean':[
+            matrix.mean(axis=0).tolist(), # column wise mean
+            matrix.mean(axis=1).tolist(), # row wise mean
+            matrix.mean().tolist()
+        ],
+        'variance':[
+            matrix.var(axis=0).tolist(),
+            matrix.var(axis=1).tolist(),
+            matrix.var().tolist()
+        ],
+        'standard deviation':[
+            matrix.std(axis=0).tolist(),
+            matrix.std(axis=1).tolist(),
+            matrix.std().tolist()
+        ],
+        'max':[
+            matrix.max(axis=0).tolist(),
+            matrix.max(axis=1).tolist(),
+            matrix.max().tolist()
 
-
-
+        ],
+        'min':[
+            matrix.min(axis=0).tolist(),
+            matrix.min(axis=1).tolist(),
+            matrix.min().tolist()
+        ],
+        'sum':[
+            matrix.sum(axis=0).tolist(),
+            matrix.sum(axis=1).tolist(),
+            matrix.sum().tolist()
+        ]
+    }
 
     return calculations
